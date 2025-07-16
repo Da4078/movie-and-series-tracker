2420dfa83521308bdec43f165a3b74bc4f4a8f6d
Daniel Maric <danielmaric48@gmail.com>
2025-07-16 14:04:20 +0200

    Spring Boot + Docker + PostgreSQL setup

---
+++ src/main/java/org/daniel/moviesandseriestrackermaster/MoviesAndSeriesTrackerMasterApplication.java
@@ -0,0 +1,13 @@
+package org.daniel.moviesandseriestrackermaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesAndSeriesTrackerMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesAndSeriesTrackerMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/daniel/moviesandseriestrackermaster/MoviesAndSeriesTrackerMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daniel.moviesandseriestrackermaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesAndSeriesTrackerMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
